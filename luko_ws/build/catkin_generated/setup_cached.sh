118cfadd29bf013d5704287d81fad58bc08cd8ca
ICRS Eurobot Team <eurobot@icrs.io>
2017-10-27 13:33:58 +0000

    created ROS workspace

---
+++ luko_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/pi/luko/luko_ws/devel:/opt/ros/kinetic:/usr/"
+export LD_LIBRARY_PATH="/opt/ros/kinetic/lib:/usr/lib:/usr/lib/arm-linux-gnueabihf"
+export PKG_CONFIG_PATH="/opt/ros/kinetic/lib/pkgconfig:/usr/lib/pkgconfig:/usr/lib/arm-linux-gnueabihf/pkgconfig"
+export PWD="/home/pi/luko/luko_ws/build"
+export PYTHONPATH="/opt/ros/kinetic/lib/python2.7/dist-packages:/usr/lib/python2.7/dist-packages"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/pi/luko/luko_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/pi/luko/luko_ws/src:$ROS_PACKAGE_PATH"
