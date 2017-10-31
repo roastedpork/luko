c3903abbc1e707c197554bd0faa5bfc6cc91292a
luko <al2114@ic.ac.uk>
2017-10-31 11:44:45 +0000

    removed rosserial

--- luko_ws/build/rosserial/rosserial/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/luko/luko_ws/src/rosserial/rosserial
-# Build directory: /home/pi/luko/luko_ws/build/rosserial/rosserial
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
