ed099572ec1331e24b8d738aae094bd1b77a2446
luko <al2114@ic.ac.uk>
2017-11-09 23:10:19 +0000

    calibrated all joints

--- mbed/Servo.h
+++ mbed/Servo.h
@@ -2,44 +2,53 @@
 #define SERVO_H
 
 // Joint 1 Parameters
+// With analogin passing through 1k ohms 
 #define J1_LOWER 0
 #define J1_UPPER 180
 #define J1_PIN   p20
 #define J1_POUT  p21
-#define J1_GRAD  0.00318
-#define J1_INTC  0.126
+#define J1_GRAD  0.00339
+#define J1_INTC  0.13722
 
 // Joint 2 Parameters
-#define J2_LOWER 0
-#define J2_UPPER 180
+// With analogin passing through 1k ohms 
+// analog conversion accurate in range [3,167]
+#define J2_LOWER 47
+#define J2_UPPER 94
 #define J2_PIN   p19
 #define J2_POUT  p22
-#define J2_GRAD  0.00318
-#define J2_INTC  0.126
+#define J2_GRAD  0.00339
+#define J2_INTC  0.170
 
 // Joint 3 Parameters
-#define J3_LOWER 0
-#define J3_UPPER 180
+// With analogin passing through 1k ohms 
+// analog conversion accurate in range [5,160]
+#define J3_LOWER 10
+#define J3_UPPER 110
 #define J3_PIN   p18
 #define J3_POUT  p23
-#define J3_GRAD  0.00318
-#define J3_INTC  0.126
+#define J3_GRAD  0.00328
+#define J3_INTC  0.13036
 
 // Joint 4 Parameters
-#define J4_LOWER 0
-#define J4_UPPER 180
+// With analogin passing through 1k ohms 
+// analog conversion accurate in range [5,160]
+#define J4_LOWER 5
+#define J4_UPPER 140
 #define J4_PIN   p17
 #define J4_POUT  p24
-#define J4_GRAD  0.00318
-#define J4_INTC  0.126
+#define J4_GRAD  0.00324
+#define J4_INTC  0.12868
 
 // Joint 5 Parameters
+// With analogin passing through 1k ohms 
+// analog conversion accurate in range [1,168]
 #define J5_LOWER 0
 #define J5_UPPER 180
 #define J5_PIN   p16
 #define J5_POUT  p25
-#define J5_GRAD  0.00318
-#define J5_INTC  0.126
+#define J5_GRAD  0.00343
+#define J5_INTC  0.16594
 
 // Servo Motion Resolution
 #define STEP_TIME 50 // 50ms
--- mbed/main.cpp
+++ mbed/main.cpp
@@ -41,23 +41,32 @@ int main() {
     serial_thread.start(SerialReader::getValue);
     myled = 1;
 
-    const int start = 45;
-    const int end = 135;    
-    joints[0].setAngle(start);
-    joints[1].setAngle(start);
-    wait_ms(500);
 
-    joints[0].setThreadParams(end,STEP_FAST);
-    joints[1].setThreadParams(end,STEP_FAST);
-    mthreads[0].start(&joints[0],&Servo::setAngle_threaded);
-    mthreads[1].start(&joints[1],&Servo::setAngle_threaded);
+//    const int start = 45;
+//    const int end = 5;    
+//    joints[0].setAngle(90);
+//    joints[1].setAngle(start);
+//    joints[2].setAngle(20);
+//    joints[3].setAngle(90);
+    joints[4].setAngle(90);
+//    wait_ms(1500);
+
+
+//    joints[0].setThreadParams(end,STEP_FAST);
+//    joints[1].setThreadParams(end,STEP_FAST);
+//    joints[2].setThreadParams(end,STEP_FAST);
+//    mthreads[0].start(&joints[0],&Servo::setAngle_threaded);
+//    mthreads[1].start(&joints[1],&Servo::setAngle_threaded);
+//    mthreads[2].start(&joints[2],&Servo::setAngle_threaded);
+//    mthreads[3].start(&joints[3],&Servo::setAngle_threaded);
+//    mthreads[4].start(&joints[4],&Servo::setAngle_threaded);
     
 //    pc.printf("[0] Total: %d, Free: %d, Used: %d\n\r", mthreads[0].stack_size(),mthreads[0].free_stack(),mthreads[0].used_stack());
 //    pc.printf("[1] Total: %d, Free: %d, Used: %d\n\r", mthreads[1].stack_size(),mthreads[1].free_stack(),mthreads[1].used_stack());
 //    pc.printf("[2] Total: %d, Free: %d, Used: %d\n\r", mthreads[2].stack_size(),mthreads[2].free_stack(),mthreads[2].used_stack());
 //    pc.printf("[3] Total: %d, Free: %d, Used: %d\n\r", mthreads[3].stack_size(),mthreads[3].free_stack(),mthreads[3].used_stack());
 //    pc.printf("[4] Total: %d, Free: %d, Used: %d\n\r", mthreads[4].stack_size(),mthreads[4].free_stack(),mthreads[4].used_stack());
-    
+
     while(1){
         
 
@@ -65,7 +74,6 @@ int main() {
             myled = !myled;
             std::memcpy(tgt_joints,SerialReader::value,5*sizeof(uint8_t));
             SerialReader::read_ready = false;
-            
             for(unsigned i = 0;i<5;i++){
                 if (joints[i].inMotion) mthreads[i].terminate();
                 joints[i].setThreadParams(tgt_joints[i], STEP_FAST);
@@ -75,45 +83,66 @@ int main() {
         
         for(unsigned i=0; i<5; i++) cur_joints[i] = joints[i].getAngle();
         
+        pc.printf("now:<%02x%02x%02x%02x%02x>\n\r",
+                    cur_joints[0],cur_joints[1],cur_joints[2],
+                    cur_joints[3],cur_joints[4]);
         SerialReader::io.printf("now:<%02x%02x%02x%02x%02x>\n\r",
                     cur_joints[0],cur_joints[1],cur_joints[2],
                     cur_joints[3],cur_joints[4]);
         wait_ms(100);
     }  
 
-
-        
-        // for future servo calibration
-//        while (traverse){
-//            if (traverse >= 5) current += step_a;
-//            else if (traverse <= -5) current -= step_a;
-//            else current += traverse;
-//            
-//            bool arrived = false;
-//            traverse = end-current;
-//            joints[0].setAngle(current);
-//            wait_ms(step_t);
-//            
-//            if (abs(current-joints[0].getAngle()) <= 3) arrived = true; 
-//            pc.printf("Traverse: %03d , current: %d, arr:%d\n\r", traverse, current, (arrived) ? 1 : 0);            
-//        }
-        
-       // for(int i = 1;i<=10;i++){
-//                
-//                uint8_t arrived = 0;
+        // Calibration round 1 (angle vs analog readout)
+//        int start = 120;
+//        joints[4].setAngle(start);
+//        wait_ms(2000);
+//        
+//        for(int target = start;target<=start+60;target++){
 //
-//                joints[0].setAngle(target);
+//                joints[4].setAngle(target);
 //                wait_ms(50);
 //                
-//                int actual = joints[0].getAngle();
-//                if (abs(actual-target) <= 3){
-//                    arrived = 1;
-//                }
+//                float actual = joints[4].getAnalog();
+//            
 //                
-//                pc.printf("Target: %03d, arrived: %d\n\r", target, arrived);
+//                pc.printf("Target, %03d, actual, %0.06f\r\n", target, actual);
+//        }
+  
+    
+        // calibration round 2 (angle vs processed)
+//        int start = 0;
+//        joints[4].setAngle(start);
+//        wait_ms(1000);
+//
+//        for(int target = start;target<=start+ 60;target++){
+//
+//                joints[4].setAngle(target);
+//                wait_ms(50);
+//                
+//                int actual = joints[4].getAngle();
+//            
+//                
+//                pc.printf("Target, %03d, diff, %d\r\n", target, actual-target);
+//        }
+
+        // calibration round 3 (angle vs processed on frame)
+//        joints[4].setAngle(0);
+//        wait_ms(2000);
+//
+//        for(int target = 0;target<=180;target++){
+//
+//                joints[4].setAngle(target);
+//                wait_ms(50);
+//                
+//                int actual = joints[4].getAngle();
+//            
+//                
+//                pc.printf("Target, %03d, diff, %d\r\n", target, actual-target);
 //        }
   
         
   
         return 0;
 }
+
+
