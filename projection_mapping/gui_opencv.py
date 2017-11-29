91f972550540be37ee212f12f9d444fe1506e54a
luko <al2114@ic.ac.uk>
2017-11-29 13:44:54 +0000

    transform works nicely with angle

--- projection_mapping/gui_opencv.py
+++ projection_mapping/gui_opencv.py
@@ -1,78 +1,99 @@
 #!/usr/bin/env python3
 import sys
 import time
+import math
 import numpy as np
 import pygame
 import cv2
 
 # dimension of the display
-screen_cols = 1280
-screen_rows = 720
+screen_cols = 1182 #1280
+screen_rows = 624 #720
+
+# initialise GUI environment
+pygame.init()
+flags = pygame.DOUBLEBUF | pygame.HWSURFACE | pygame.NOFRAME | pygame.FULLSCREEN
+screen = pygame.display.set_mode((screen_cols,screen_rows), flags)
+screen.fill([0,0,0])
+pygame.mouse.set_visible(False)
+pygame.display.set_caption("Luko")
+clock = pygame.time.Clock()
+info = pygame.display.Info()
+print(info)
 
 
-def processImage(img):
-	# converts an OpenCV image into a pygame image
-	img = np.rot90(img)
-	size = img.shape
-	short_side = min(screen_cols,screen_rows)
-	if len(size) == 3:
-		img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-	new_size = (int(size[1]*short_side/max(size)), int(size[0]*short_side/max(size)))
-
-	return cv2.resize(img, dsize=new_size)
 
 
-def transformImage(img, angle):
-	# performs perspective transform on an image depending on angle
-	size = img.shape
-	print(size)
-	rows, cols = size[0], size[1]
+class ImageHandler(object):
+	def __init__(self, file, grayscale = False):
 
-	# Generate map matrix
-	pts1 = np.float32([[0,0],[cols,0],[cols,rows],[0,rows]])	# src
-	pts2 = np.float32([[100,0],[cols-100,0],[cols,rows],[0,rows]])	# dst
-	M = cv2.getPerspectiveTransform(pts1,pts2)
+		self.original = cv2.imread(file, 0) if grayscale else cv2.imread(file,1)
+		self.buffer = np.copy(self.original)
 
-	return cv2.warpPerspective(img,M,(cols,rows))
+	def transform(self,theta):
+		# performs perspective transform on an OpenCV image depending on angle
+		size = self.original.shape
+		cols, rows = size[0], size[1]
+		
+		# empirically obtained scaling factors
+		scaleC = 0.25
+		scaleR = 0.5	
 
-def calcBlitPoint(img):
-	# calculates the top left (?) position to centre the image onto the 720x720 display
-	size = img.shape
-	cols, rows = size[0],size[1]
-	return ((screen_cols-cols)/2,(screen_rows-rows)/2)
+		delC = int(scaleC*cols*(math.sin(math.radians(theta)))/2)
+		delR = int(rows*scaleR*math.sin(math.radians(theta)))
+
+		# Generate map matrix
+		pts1 = np.float32([[0,0],[cols,0],[cols,rows],[0,rows]])			# src
+		pts2 = np.float32([[delC,0],[cols-delC,0],[cols,rows-delR],[0,rows-delR]])	# dst
+		M = cv2.getPerspectiveTransform(pts1,pts2)
+
+		# Write to buffer
+		self.buffer = cv2.warpPerspective(self.original,M,(cols,rows-delR))
+
+	def display(self, screen, blitPoint = None):
+		# converts an OpenCV image buffer into a resized pygame image
+		self.buffer = np.rot90(self.buffer)
+		size = self.buffer.shape
+		cols, rows = size[0],size[1]
+
+		short_side = min(info.current_w,info.current_h)
+		if len(size) == 3:
+			self.buffer = cv2.cvtColor(self.buffer, cv2.COLOR_BGR2RGB)
+		new_size = (int(size[1]*short_side/max(size)), int(size[0]*short_side/max(size)))
+
+		self.buffer = cv2.resize(self.buffer, dsize=new_size)
+		
+		# display image on screen
+		frame = pygame.surfarray.make_surface(self.buffer)
+		if blitPoint == None: blitPoint = ((info.current_w-cols)/2, (info.current_h-rows)/2)
+		screen.blit(frame, blitPoint)
+		print(info.current_w,info.current_h)
+		print(self.buffer.shape)
+		print(blitPoint)
+
 
 if __name__ == "__main__":
-
-	# initialise GUI environment
-	pygame.init()
-	flags = pygame.DOUBLEBUF | pygame.HWSURFACE #| pygame.NOFRAME | pygame.FULLSCREEN
-	screen = pygame.display.set_mode((screen_cols,screen_rows), flags)
-	screen.fill([0,0,0])
-	pygame.display.set_caption("Luko")
-	clock = pygame.time.Clock()
-
 	# load cv2 image
-	#img = cv2.imread('dog_bg_black.jpg',1)
-	img = cv2.imread('grid_rect.png',1)
-	img = transformImage(img,0)
-	img = processImage(img)
-
-	frame = pygame.surfarray.make_surface(img)
-	
-	screen.blit(frame, calcBlitPoint(img))
-	#screen.blit(frame, (0,0))
+	img = ImageHandler('grid30.png')
+	img.transform(30)
+	img.display(screen)
 	pygame.display.update()
 
 	# main running loop
 	try:
+		angle = 0
 		while True:
 			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
+				if event.type == pygame.KEYDOWN:
 					sys.exit(0)
 				print(event)
 
+			img.transform(angle)
+			screen.fill([0,0,0])
+			img.display(screen)
+			angle = angle + 5 if angle < 90 else 0
 			pygame.display.update()
-			clock.tick(60)
+			clock.tick(1)
 
 	except KeyboardInterrupt or SystemExit:
 		pygame.quit()
