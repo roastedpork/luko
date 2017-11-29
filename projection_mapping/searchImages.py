cbe25d07e40e345bd62f69c451c363157e2745d5
luko <al2114@ic.ac.uk>
2017-11-29 19:52:48 +0000

    HIGH RES SCRAPPING WORKS

--- projection_mapping/searchImages.py
+++ projection_mapping/searchImages.py
@@ -14,7 +14,7 @@ def get_links(query_string, num_images):
     links = []
     #step by 100 because each return gives up to 100 links
     for i in range(0,num_images,100):
-        url = 'https://www.google.com/search?q='+query_string#+'\
+        url = 'https://www.google.com/search?q='+query_string+'&tbm=isch&source=lnms'
         #&tbm=isch&ved=0ahUKEwjjovnD7sjWAhUGQyYKHTmrC2kQuT0I7gEoAQ&start='+str(i)+'\
         #&yv=2&vet=10ahUKEwjjovnD7sjWAhUGQyYKHTmrC2kQuT0I7gEoAQ.1m7NWePfFYaGmQG51q7IBg.i&ijn=1&asearch=ichunk&async=_id:rg_s,_pms:s'
 
@@ -26,10 +26,12 @@ def get_links(query_string, num_images):
         html = urllib.request.urlopen(request).read().decode('utf-8')
 
         #use BeautifulSoup to parse as html
-        new_soup = Soup(html,'html5lib')
+        soup = Soup(html,'html.parser')
+        for a in soup.find_all('img'):
+            print(a)
 
         #all img tags, only returns results of search
-        imgs = new_soup.find_all('img')
+        imgs = soup.find_all('img')
 
         #loop through images and put src in links list
         for j in range(len(imgs)):
@@ -41,8 +43,8 @@ def get_links(query_string, num_images):
 def search_images(term,num_images):
     img_links = get_links(term,num_images)
 
-    for i,l in enumerate(img_links):
-        urllib.request.urlretrieve(l, "images/"+term+"_%02d.jpg"%(i))
+    #for i,l in enumerate(img_links):
+    #    urllib.request.urlretrieve(l, "images/"+term+"_%02d.jpg"%(i))
 
 if __name__ == '__main__':
     term = "cat"
