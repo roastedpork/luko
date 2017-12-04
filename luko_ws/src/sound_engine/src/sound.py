9ffc2167e4b04eadcf89971a01955c5f0cc73b60
Ben Withers <manicben20@gmail.com>
2017-12-04 16:22:28 +0000

    Converted sound engine into ROS node

--- luko_ws/src/sound_engine/src/sound.py
+++ luko_ws/src/sound_engine/src/sound.py
@@ -7,76 +7,83 @@ import os
 import numpy as np
 from pydub import AudioSegment
 
+import rospy
+from std_msgs.msg import String
+
 intent_map = {
-        'hey_luko'                      : ['hey.wav', 4],
-        'smalltalk.agent.acquaintance'  : ['acq.wav', 4],
-        'smalltalk.agent.age'           : ['age.wav', 4],
-        'smalltalk.agent.birth\_date'   : ['dob.wav', 4],
-        'smalltalk.agent.busy'          : ['busy.wav', 4],
-        'smalltalk.agent.can\_you\_help': ['help.wav', 3],
-        'smalltalk.agent.chatbot'       : ['chatbot.wav', 5],
-        'smalltalk.agent.hungry'        : ['hungry.wav', 6],
-        'smalltalk.agent.marry\_user'   : ['marry.wav', 3],
-        'smalltalk.agent.my\_friend'    : ['friend.wav', 3],
-        'smalltalk.dialog.how\_are\_you': ['dhay.wav', 4],
-        'smalltalk.agent.real'          : ['real.wav', 3],
-        'smalltalk.agent.there'         : ['there.wav', 4],
-        'smalltalk.appraisal.good'      : ['good.wav', 2],
-        'smalltalk.appraisal.no\_problem': ['np.wav', 3],
-        'smalltalk.appraisal.thank\_you': ['thx.wav', 3],
-        'smalltalk.appraisal.well\_done': ['wd.wav', 2],
-        'smalltalk.appraisal.welcome'   : ['welcome.wav', 3],
-        'smalltalk.dialog.i\_do\_not\_care': ['idc.wav', 5],
-        'smalltalk.dialog.sorry'        : ['sorry.wav', 3],
-        'smalltalk.dialog.what\_do\_you\_mean': ['wdym.wav', 4],
-        'smalltalk.dialog.wrong'        : ['wrong.wav', 5],
-        'smalltalk.emotions.ha\_ha'     : ['haha.wav', 2],
-        'smalltalk.emotions.wow'        : ['wow.wav', 3],
-        'smalltalk.greetings.bye'       : ['bye.wav', 3],
-        'smalltalk.greetings.goodevening': ['ge.wav', 4],
-        'smalltalk.greetings.goodmorning': ['gm.wav', 4],
-        'smalltalk.greetings.goodnight' : ['gn.wav', 3],
-        'smalltalk.greetings.hello'     : ['hello.wav', 4],
-        'smalltalk.greetings.how\_are\_you': ['ghay.wav', 4],
-        'smalltalk.greetings.nice\_to\_meet\_you': ['ntmy.wav', 3],
-        'smalltalk.greetings.nice\_to\_talk\_to\_you': ['nttty.wav', 3],
-        'smalltalk.greetings.whatsup'   : ['whatsup.wav', 3],
-        'smalltalk.user.bored'          : ['bored.wav', 5],
-        'smalltalk.user.happy'          : ['happy.wav', 2],
-        'smalltalk.user.sad'            : ['sad.wav', 6],
-        'smalltalk.user.sleepy'         : ['sleepy.wav', 5],
-        'smalltalk.user.testing\_agent' : ['test.wav', 2],
-        'test_intent'                   : ['a.wav', 3],
-        'default'                       : ['e.wav', 3]
+        'hey_luko'            : ['hey.wav', 4],
+        'agent.acquaintance'  : ['acq.wav', 4],
+        'agent.age'           : ['age.wav', 4],
+        'agent.birth\_date'   : ['dob.wav', 4],
+        'agent.busy'          : ['busy.wav', 4],
+        'agent.can\_you\_help': ['help.wav', 3],
+        'agent.chatbot'       : ['chatbot.wav', 5],
+        'agent.hungry'        : ['hungry.wav', 6],
+        'agent.marry\_user'   : ['marry.wav', 3],
+        'agent.my\_friend'    : ['friend.wav', 3],
+        'dialog.how\_are\_you': ['dhay.wav', 4],
+        'agent.real'          : ['real.wav', 3],
+        'agent.there'         : ['there.wav', 4],
+        'appraisal.good'      : ['good.wav', 2],
+        'appraisal.no\_problem': ['np.wav', 3],
+        'appraisal.thank\_you': ['thx.wav', 3],
+        'appraisal.well\_done': ['wd.wav', 2],
+        'appraisal.welcome'   : ['welcome.wav', 3],
+        'dialog.i\_do\_not\_care': ['idc.wav', 5],
+        'dialog.sorry'        : ['sorry.wav', 3],
+        'dialog.what\_do\_you\_mean': ['wdym.wav', 4],
+        'dialog.wrong'        : ['wrong.wav', 5],
+        'emotions.ha\_ha'     : ['haha.wav', 2],
+        'emotions.wow'        : ['wow.wav', 3],
+        'greetings.bye'       : ['bye.wav', 3],
+        'greetings.goodevening': ['ge.wav', 4],
+        'greetings.goodmorning': ['gm.wav', 4],
+        'greetings.goodnight' : ['gn.wav', 3],
+        'greetings.hello'     : ['hello.wav', 4],
+        'greetings.how\_are\_you': ['ghay.wav', 4],
+        'greetings.nice\_to\_meet\_you': ['ntmy.wav', 3],
+        'greetings.nice\_to\_talk\_to\_you': ['nttty.wav', 3],
+        'greetings.whatsup'   : ['whatsup.wav', 3],
+        'user.bored'          : ['bored.wav', 5],
+        'user.happy'          : ['happy.wav', 2],
+        'user.sad'            : ['sad.wav', 6],
+        'user.sleepy'         : ['sleepy.wav', 5],
+        'user.testing\_agent' : ['test.wav', 2],
+        'test_intent'         : ['a.wav', 3],
+        'default'             : ['e.wav', 3]
     }
 
+class SoundEngine(object):
+    def __init__(self):
+        # initialize pubsub topics
+        self.sub_play = rospy.Subscriber("smalltalk", String, self.callback_play, queue_size = 1)
 
-def play(intent):
+    def callback_play(self,intent):
 
-    ''' Full implementation disabled for now until all sound files present
-    try:
-        audio_file = intent_map[intent][0]
-        rate = intent_map[intent][1] + (randint(-9,9)/10)
-    except KeyError:
+        ''' Full implementation disabled for now until all sound files present
+        try:
+            audio_file = intent_map[intent][0]
+            rate = intent_map[intent][1] + (randint(-9,9)/10)
+        except KeyError:
+            audio_file = intent_map["default"][0]
+            rate = intent_map["default"][1] + (randint(-9,9)/10)
+        '''
+
         audio_file = intent_map["default"][0]
         rate = intent_map["default"][1] + (randint(-9,9)/10)
-    '''
 
-    audio_file = intent_map["default"][0]
-    rate = intent_map["default"][1] + (randint(-9,9)/10)
+        song = AudioSegment.from_wav("sounds/" + audio_file)
 
-    song = AudioSegment.from_wav("sounds/" + audio_file)
+        samples = song.get_array_of_samples()
 
-    samples = song.get_array_of_samples()
+        sampled = np.delete(samples, np.arange(0, len(samples), rate))
 
-    sampled = np.delete(samples, np.arange(0, len(samples), rate))
+        result = song._spawn(array.array(song.array_type, sampled))
 
-    result = song._spawn(array.array(song.array_type, sampled))
+        filename = "luko_sound_" + str(rate) + ".wav"
 
-    filename = "luko_sound_" + str(rate) + ".wav"
+        result.export(filename, format="wav")
 
-    result.export(filename, format="wav")
-
-    # Play generated sound file and then delete the file
-    os.system("aplay " + filename)
-    os.remove(filename)
+        # Play generated sound file and then delete the file
+        os.system("aplay " + filename)
+        os.remove(filename)
