eb8c346af37d0d3a541464c9208211e25d8350a4
luko <al2114@ic.ac.uk>
2017-12-10 21:42:40 +0000

    updated pinchtozoom

--- luko_ws/src/cv/src/handrec_pinch.py
+++ luko_ws/src/cv/src/handrec_pinch.py
@@ -57,49 +57,28 @@ class cv:
                 img, contours, _ = cv2.findContours(skinMask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
                 if len(contours) > 0:
                     try:
-                        max_cnt  = sorted(contours,key = cv2.contourArea, reverse=True)[0]
-                        max_cnt2 = sorted(contours,key = cv2.contourArea, reverse=True)[2] 
+                        sortedContours = sorted(contours,key = cv2.contourArea, reverse=True) 
+                        max_cnt  = sortedContours[0]
+                        max_cnt2 = sortedContours[1]
                         M1 = cv2.moments(max_cnt)
                         M2 = cv2.moments(max_cnt2)
                         cx1 = int(M1['m10']/M1['m00'])
                         cy1 = int(M1['m01']/M1['m00'])
                         cx2 = int(M2['m10']/M2['m00'])
                         cy2 = int(M2['m01']/M2['m00'])
+                        dx = cx2 - cx1
+                        dy = cy2 - cy1
 
-                        peri = cv2.arcLength(max_cnt,True)
-                        approx = cv2.approxPolyDP(max_cnt, 0.005*peri, True)
-                        hull = cv2.convexHull(approx)
-                        
-                        minDist = np.inf
-                        minIndex = -1
-                        for i in range(len(hull)):
-                            cv2.circle(frame,tuple(hull[i,0,:]),3,(0,255,255),-1)
-                            dist = self.findDistance(hull[i,0,:], hull[(i+1)%len(hull),0,:])
-                            if minDist > dist:
-                                minDist = dist
-                                minIndex = i
+                        #coords of mid point between hands
+                        mpx = cx1+0.5*dx
+                        mpy = cy1+0.5*dy
 
-                        offsetVec = hull[minIndex,0,:]-np.array([CAM_WIDTH/2, CAM_HEIGHT/2])
-                        print offsetVec*SCALING_FACTOR_D
-                        phi = np.arctan2(offsetVec[1],offsetVec[0])
-                        print np.degrees(phi)
-
-                        offsetDist = np.linalg.norm(offsetVec)*SCALING_FACTOR_D
-                        # print offsetDist
-                        # cosine rule
-                        theta = np.pi/2 - phi
-                        new_luko_dist = np.sqrt(offsetDist*offsetDist + LUKO_DISTANCE*LUKO_DISTANCE - 2*offsetDist*LUKO_DISTANCE*np.cos(theta))
-                        print theta, new_luko_dist
-                        delta_rot = np.arcsin(np.sin(theta)/new_luko_dist * offsetDist)
-                        delta_y = new_luko_dist - LUKO_DISTANCE
-                        print np.degrees(delta_rot),delta_y
-                        
                         msg = Radial()
                         msg.radius = new_luko_dist
                         msg.theta = theta
                         self.pub.publish(msg)
 
-                        cv2.circle(frame,tuple(hull[minIndex,0,:]),3,(163,50,204),-1)
+                        #cv2.circle(frame,tuple(hull[minIndex,0,:]),3,(163,50,204),-1)
                         cv2.circle(frame, (cx,cy), 3, (0,0,255), -1)
                         cv2.drawContours(frame,[approx],0,(0,255,0),1)
 
@@ -108,6 +87,11 @@ class cv:
                     except Exception as e:
                         calc = False
                         print e
+
+                handDist = np.sqrt(np.power(dx, 2)+ np.power (dy, 2))
+                if handDist < 50
+                    sleep(50)
+
 
                 # Operations on the frame
                 skin = cv2.bitwise_and(frame, frame, mask = skinMask)
@@ -125,24 +109,9 @@ class cv:
                 if  interrupt & 0xFF == ord('q'):
                     break
 
-  #rospy.init_node('luko_cv', anonymous = True)
-    #rate = rospy.Rate(1)
-    #while not rospy.is_shutdown():
-            #hello_str = "hello world %s" % rospy.get_time()
-            #rospy.loginfo(hello_str)
-            #pub.publish(hello_str)
-            #rate.sleep()
 
 if __name__ == '__main__':
     rospy.init_node('luko_cv', anonymous = True)
     vision = cv()
     vision.run()
 
-#try:
- #   while(1):
-  #      calc = False
-       
-#except KeyboardInterrupt, TypeError:
- #   # Release camera & end program
-  #  camera.release()
-   # cv2.destroyAllWindows()
