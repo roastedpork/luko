404325cefbf89951be7fc7206b56a78ad03c2ca6
luko <al2114@ic.ac.uk>
2017-12-10 22:38:30 +0000

    handpinch, doesn't work

--- luko_ws/src/cv/src/handrec_pinch.py
+++ luko_ws/src/cv/src/handrec_pinch.py
@@ -38,8 +38,8 @@ class cv:
         while not rospy.is_shutdown():
      # Clear the IO of the previous buffer
             stream = io.BytesIO()
-			open = 0
-			closed = 0
+            handOpen = 0
+            handClosed = 0
             for raw in camera.capture_continuous(stream,format='jpeg'):
                 frame = cv2.imdecode(np.fromstring(stream.getvalue(),dtype=np.uint8),1)
                 stream.truncate()
@@ -59,29 +59,27 @@ class cv:
                 img, contours, _ = cv2.findContours(skinMask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
                 if len(contours) > 0:
                     try:
-                        sortedContours = sorted(contours,key = cv2.contourArea, reverse=True) 
+                        sortedContours = sorted(contours,key = cv2.contourArea, reverse=True)
                         max_cnt  = sortedContours[0]
-						max_cnt2 = sortedContours[1]
-                        
-						M1 = cv2.moments(max_cnt)
-						cx1 = int(M1['m10']/M1['m00'])
-						cy1 = int(M1['m01']/M1['m00'])
+			max_cnt2 = sortedContours[1]
+			M1 = cv2.moments(max_cnt)
+			cx1 = int(M1['m10']/M1['m00'])
+			cy1 = int(M1['m01']/M1['m00'])
 
-						if cv2.contourArea(max_cnt2) > 0.7*cv2.contourArea(max_cnt)
-							M2 = cv2.moments(max_cnt2)
-							cx2 = int(M2['m10']/M2['m00'])						
-							cy2 = int(M2['m01']/M2['m00'])
-							dx = cx2 - cx1
-							dy = cy2 - cy1							
-							#coords of mid point between hands
-							mpx = cx1+0.5*dx
-							mpy = cy1+0.5*dy
+                        if cv2.contourArea(max_cnt2) > 0.7*cv2.contourArea(max_cnt):
+                            M2 = cv2.moments(max_cnt2)
+                            cx2 = int(M2['m10']/M2['m00'])
+                            cy2 = int(M2['m01']/M2['m00'])
+                            dx = cx2 - cx1
+                            dy = cy2 - cy1
+                            #coords of mid point between hands
+                            mpx = cx1+0.5*dx
+                            mpy = cy1+0.5*dy
 
-							cv2.circle(frame, (cx2,cy2), 3, (0,0,255), -1)
-							#cv2.drawContours(frame,[approx],0,(0,255,0),1)
+                            cv2.circle(frame, (cx2,cy2), 3, (0,0,255), -1)
+                            #cv2.drawContours(frame,[approx],0,(0,255,0),1)
 
-						
-						#msg = Radial()
+			#msg = Radial()
                         #msg.radius = new_luko_dist
                         #msg.theta = theta
                         #self.pub.publish(msg)
@@ -95,36 +93,36 @@ class cv:
                         calc = False
                         print e
 
-                handDist = np.sqrt(np.power(dx, 2)+ np.power (dy, 2))				
-				
-				if handDist < 50 #if hand closed
-					closed = 1
-					if open == 1
-						zoomIn = 1
-						open = 0
-                    sleep(50)
-					
-				if handDist > 100 #if hand open
-					open = 1
-					if closed == 1
-						zoomOut = 1                
-						closed = 0
-					sleep(50)
-				
-				print open
-				print closed
-				print zoonIn
-				print zoomOut
+                handDist = np.sqrt(np.power(dx, 2)+ np.power (dy, 2))
+                if handDist < 50: #if hand closed
+                    handClosed = 1
+                    if handOpen == 1: #if hand was open 500ms ago
+                        zoomOut = 1
+                        handOpen = 0
+                    sleep(500)
 
-				
-				# Operations on the frame
+                if handDist > 100: #if hand open    
+                    handOpen = 1
+                    if handClosed == 1: #if hand was closed 500ms ago
+                        zoomIn = 1
+                        handClosed = 0
+                    sleep(500)
+
+                print handOpen
+                print handClosed
+                print zoonIn
+                print zoomOut
+                zoomIn = 0
+                zoomOut = 0
+
+		# Operations on the frame
                 skin = cv2.bitwise_and(frame, frame, mask = skinMask)
                 if len(contours) > 0 and calc:
                     cv2.circle(skin, (cx1,cy1), 3, (0,0,255))
                     cv2.circle(skin, (cx2,cy2), 3, (0,0,255))
-				skin=cv2.flip(skin,1)
+		skin=cv2.flip(skin,1)
                 # Display frame in a window
-                #cv2.imshow('Frame',frame)
+                cv2.imshow('Frame',frame)
                 cv2.imshow('Filtered Frame',skin)
                 #cv2.imshow('mask',skinMask)
                 #interrupt=cv2.waitKey(1)
