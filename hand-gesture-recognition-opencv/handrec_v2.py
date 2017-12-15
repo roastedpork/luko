775fe2e3e411991c3aa0250266e868bc03751a26
luko <al2114@ic.ac.uk>
2017-12-15 22:21:53 +0000

    Added CV

---
+++ hand-gesture-recognition-opencv/handrec_v2.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+
+
+import io
+import cv2
+import numpy as np
+import math
+import time
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+
+# Camera
+camera = PiCamera(resolution = (320,240))
+rawCapture = PiRGBArray(camera)
+time.sleep(0.1)
+
+# HSV colour tolerances
+lower = np.array([0,30,50], dtype=np.uint8)
+upper = np.array([30,255,255], dtype=np.uint8)
+
+
+try:
+
+    while(1):
+        calc = False
+        # Clear the IO of the previos buffer
+        stream = io.BytesIO()
+        for raw in camera.capture_continuous(stream,format='jpeg'):
+            frame = cv2.imdecode(np.fromstring(stream.getvalue(),dtype=np.uint8),1)
+            stream.truncate()
+            stream.seek(0)
+
+            # Convert to HSV colour space
+            frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            skinMask = cv2.inRange(frame_hsv, lower, upper)
+
+            # apply erosion, dilation and blurring to mask
+            kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(7,7))
+            skinMask = cv2.erode(skinMask, kernel, iterations=1)
+            skinMask = cv2.dilate(skinMask, kernel, iterations=1)
+            skinMask = cv2.GaussianBlur(skinMask, (3,3), 0)
+            
+            # calculate centroid
+            img, contours, _ = cv2.findContours(skinMask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+            if len(contours) > 0:
+                try:
+                    max_cnt = sorted(contours,key = cv2.contourArea, reverse=True)[0]
+                    M = cv2.moments(max_cnt)
+                    cx = int(M['m10']/M['m00'])  
+                    cy = int(M['m01']/M['m00'])
+                    
+                    peri = cv2.arcLength(max_cnt,True)
+                    approx = cv2.approxPolyDP(max_cnt, 0.005*peri, True)
+                    hull = cv2.convexHull(approx)
+                    for i in range(len(hull)):
+                        cv2.circle(frame,tuple(hull[i,0,:]),3,(0,255,255),-1)
+                    cv2.circle(frame, (cx,cy), 3, (0,0,255), -1)
+                    cv2.drawContours(frame,[approx],0,(0,255,0),1)
+                    
+                    
+                    
+                    calc = True
+                except:
+                    calc = False
+            
+            # Operations on the frame
+            skin = cv2.bitwise_and(frame, frame, mask = skinMask)
+            if len(contours) > 0 and calc:
+                cv2.circle(skin, (cx,cy), 3, (0,0,255))
+                #cv2.drawContours(skin,[hull],0,(0,255,0),2)
+
+            skin=cv2.flip(skin,1)
+
+            # Display frame in a window
+            cv2.imshow('Frame',frame)
+            cv2.imshow('Filtered Frame',skin)
+            cv2.imshow('mask',skinMask)
+            interrupt=cv2.waitKey(1)
+    
+            # Quit by pressing 'q'
+            if  interrupt & 0xFF == ord('q'):
+                break
+
+except KeyboardInterrupt, TypeError:       
+    # Release camera & end program
+    camera.release()
+    cv2.destroyAllWindows()
---
+++ luko_ws/src/cv/src/handrec_swipe.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+import io
+import cv2
+import numpy as np
+import math
+import time
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import numpy as np
+
+CAM_WIDTH = 80
+CAM_HEIGHT = 60
+LUKO_HEIGHT = 35
+LUKO_DISTANCE = 20
+SCALING_FACTOR_PHI = 1.0
+SCALING_FACTOR_D = 0.1 # pixel to distance ratio
+
+# Initialise camera, allow it to warm up
+camera = PiCamera(resolution = (CAM_WIDTH,CAM_HEIGHT))
+rawCapture = PiRGBArray(camera)
+time.sleep(0.1)
+
+# HSV colour tolerances
+lower = np.array([0,30,50], dtype=np.uint8)
+upper = np.array([30,255,255], dtype=np.uint8)
+
+class cv:
+    def __init__(self):
+        self.pub = rospy.Publisher('gesture_cv', String, queue_size=10)
+
+    def findDistance(self,A,B):
+        return np.sqrt(np.power((A[0]-B[0]),2) + np.power((A[1]-B[1]),2))
+
+    def run(self):
+        while(1): #not rospy.is_shutdown():
+     # Clear the IO of the previous buffer
+            stream = io.BytesIO()
+            #lastOpen = 0
+            #lastClosed = 0
+            prevcx1 = 0
+            prevcy1 = 0
+            swipeLeft = 0
+            swipeRight = 0
+            cancelCounter = 0
+            msg = String()
+
+            for raw in camera.capture_continuous(stream,format='jpeg'):
+                frame = cv2.imdecode(np.fromstring(stream.getvalue(),dtype=np.uint8),1)
+                stream.truncate()
+                stream.seek(0)
+                #print "aaa"
+                # Convert to HSV colour space
+                frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+                skinMask = cv2.inRange(frame_hsv, lower, upper)
+
+                # apply erosion, dilation and blurring to mask
+                kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(7,7))
+                skinMask = cv2.erode(skinMask, kernel, iterations=1)
+                skinMask = cv2.dilate(skinMask, kernel, iterations=1)
+                skinMask = cv2.GaussianBlur(skinMask, (3,3), 0)
+
+                # calculate centroid
+                img, contours, _ = cv2.findContours(skinMask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+                if len(contours) > 0:
+                    try:
+                        sortedContours = sorted(contours,key = cv2.contourArea, reverse=True)
+                        max_cnt  = sortedContours[0]
+                        #if len(contours) > 1:
+                            #max_cnt2 = sortedContours[1]
+			M1 = cv2.moments(max_cnt)
+			cx1 = int(M1['m10']/M1['m00'])
+			cy1 = int(M1['m01']/M1['m00'])+2
+                        #cx1 = 0
+                        #cy1 = 0
+                        cx2 = 0
+                        cy2 = 0
+
+                if -20 < cy1 - prevcy1 < 20:
+                            if 10 < cx1 - prevcx1 < 40:
+                                swipeLeft+=1
+                                print 'swipeLeft', swipeLeft
+                                if swipeLeft == 2:
+                                    print 'SWIPE LEFT!'
+                                    msg = "SwipeLeft"
+                                    self.pub.publish(msg)
+                                    swipeLeft = 0
+
+                            elif 10 < prevcx1 - cx1 < 40:
+                                swipeRight+=1
+                                print 'swipeRight', swipeRight
+                                if swipeRight == 2:
+                                    print 'SWIPE RIGHT!'
+                                    msg = "SwipeRight"
+                                    self.pub.publish(msg)
+                                    swipeRight = 0
+                            else:
+                                cancelCounter+=1
+                                if cancelCounter == 2:
+                                    swipeLeft = 0
+                                    swipeRight = 0
+                                    cancelCounter = 0
+                        else:
+                            cancelCounter+=1
+                            if cancelCounter == 1:
+                                swipeLeft = 0
+                                swipeRight = 0
+                                cancelCounter = 0
+
+
+                        prevcx1 = cx1
+                        prevcy1 = cy1
+
+                        #cv2.circle(frame,tuple(hull[minIndex,0,:]),3,(163,50,204),-1)
+                        cv2.circle(frame, (cx1,cy1), 3, (0,0,255), -1)
+                        cv2.circle(frame, (cx2,cy2), 3, (0,0,255), -1)
+                        #cv2.drawContours(frame,[approx],0,(0,255,0),1)
+                        calc = True
+                        #print calc
+                    except Exception as e:
+                        calc = False
+                        print e
+
+                #print "about to show frame"
+		# Operations on the frame
+                skin = cv2.bitwise_and(frame, frame, mask = skinMask)
+                #if len(contours) > 0 and calc:
+                    #cv2.circle(skin, (cx1,cy1), 3, (0,0,255))
+                    #cv2.circle(skin, (cx2,cy2), 3, (0,0,255))
+		skin=cv2.flip(skin,1)
+                # Display frame in a window
+                #cv2.imshow('Frame',frame)
+                cv2.imshow('Filtered Frame',skin)
+                #cv2.imshow('mask',skinMask)
+                #interrupt=cv2.waitKey(1)
+                # Quit by pressing 'q'
+                if  interrupt & 0xFF == ord('q'):
+                    break
+
+
+if __name__ == '__main__':
+    rospy.init_node('luko_cv', anonymous = True)
+    vision = cv()
+    vision.run()
+
+#                            curr = time.time()
+#                            if curr - lastOpen > 0.7
+
+'''
+                            if handDist < 100: #if hand closed
+                                handClosed = 1
+                                prevTime = time.time()
+                                if handOpen == 1: #if hand was open 500ms ago
+                                    zoomOut = 1
+                                    handOpen = 0
+                                #time.sleep(0.25)
+                                currentTime = time.time()
+
+                            if handDist > 200: #if hand open
+                                handOpen = 1
+                                if handClosed == 1: #if hand was closed 500ms ago
+                                    zoomIn = 1
+                                    handClosed = 0
+                              #time.sleep(0.25)
+
+                            print handOpen
+                            print handClosed
+                            print zoomIn
+                            Print zoomOut
+                            zoomIn = 0
+                            zoomOut = 0
+                            handClosed = 0
+                            handOpen = 0
+'''
+			#msg = Radial()
+                        #msg.radius = new_luko_dist
+                        #msg.theta = theta
+                        #self.pub.publish(msg)
+
+
